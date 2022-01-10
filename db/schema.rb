2bc85f297878afb2a63582207dbf906c42f51ad5
dkmachine <kdstudy21@gmail.com>
2022-01-10 10:28:22 +0700

    Chapter 11: Account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_06_090458) do
+ActiveRecord::Schema.define(version: 2022_01_07_070340) do
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 2022_01_06_090458) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at", precision: 6
   end
 
 end
