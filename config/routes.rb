1a0c0d7b153e09aadbd147dff198680e27b14042
dkmachine <kdstudy21@gmail.com>
2022-01-10 15:04:31 +0700

    Chapter 12: Password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get "users/new"
     get "/help", to: "static_pages#help"
     get "/about", to: "static_pages#about"
     get "/contact", to: "static_pages#contact"
@@ -13,5 +12,6 @@ Rails.application.routes.draw do
     delete "/logout", to: "sessions#destroy"
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index show destroy)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_07_070340) do
+ActiveRecord::Schema.define(version: 2022_01_10_064312) do
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_01_07_070340) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at", precision: 6
+    t.string "reset_digest"
+    t.datetime "reset_sent_at", precision: 6
   end
 
 end
